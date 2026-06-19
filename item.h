b7d476676b6a6e4f8f2d637945c8a9b7aee07a0b
Anthonella Vegas <vegasvillalvaanthonella@gmail.com>
2026-06-19 11:52:57 -0500

    item.h

---
+++ item.h
@@ -0,0 +1,41 @@
+#ifndef __ITEM_h__
+#define __ITEM_H__
+
+#include <string>
+#include <ostream>
+using namespace std;
+
+using ID = int;
+using Priority = int;
+
+enum class ItemType { TASK, HABIT, NOTE, GOAL };
+
+class Item {
+protected:
+    ID id;
+    string title;
+    Priority priority;
+
+    static ID nextId;
+public:
+    Item(const string& title, Priority priority = 2);
+
+    virtual ~Item() = default;
+
+    ID getId() const;
+    string getTitle() const;
+    void setTitle(const string& newTitle);
+    Priority getPriority() const;
+    void setPriority(Priority p);
+
+    virtual ItemType getType() const = 0;
+    virtual bool isComplete() const = 0;
+    virtual string toString() const = 0;
+
+    bool operator<(const Item& other) const;
+
+    friend ostream& operator<<(ostream& out, const Item& item);
+
+}
+
+#endif //__ITEM_H__
