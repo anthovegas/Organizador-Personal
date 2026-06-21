fd2096ef314b4fa05d8cde6853683f0121a33fa8
Anthonella Vegas <vegasvillalvaanthonella@gmail.com>
2026-06-21 01:07:35 -0500

    task.cpp

---
+++ task.cpp
@@ -0,0 +1,58 @@
+#include "task.h"
+using namespace std;
+
+Task::Task(const string& title, Priority priority)
+    : Item (title, priority), completed(false) {}
+
+Task::~Task() {
+    for (auto i = 0; i < subtasks.size(); i++) {
+        delete subtasks[i];
+    }
+}
+
+void Task::markComplete() { completed = true; }
+
+void Task::addSubtask(Task* subtask) {
+    subtasks.push_back(subtask);
+}
+
+I Task::getSubtaskCount() const {
+    return subtasks.size();
+}
+
+ItemType Task::getType() const { return ItemType:: TASK; }
+
+B Task::isComplete() const {
+    if (!completed) return false;
+
+    for (auto i = 0; i < subtasks.size(); i++) {
+        if (!subtasks[i]->isComplete()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+I Task::countSubtasks() const {
+    I total = subtasks.size();
+
+    for (auto i = 0; i < subtasks.size(); i++) {
+        total += subtasks[i]->countSubtasks();
+    }
+    return total;
+}
+
+string Task::toString() const {
+    return toStringHelper(0);
+}
+
+string Task::toStringHelper(I level) const {
+    string indent(level * 2, ' ');
+    string check = completed ? "[x]" : "[ ]";
+    string result = indent + check + " " + title +
+                     "(prioridad " + to_string(priority) + ")\n";
+    for (auto i = 0; i < subtasks.size(); i++) {
+        result += subtasks[i]->toStringHelper(level + 1);
+    }
+    return result;
+}
